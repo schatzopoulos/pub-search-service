3da046c79a702551d7e79a3a1679e691c92d0675
serafeim <s.chatzopoulos@gmail.com>
2018-09-10 00:46:15 +0300

    Add to swagger only defined routes in a specific controller

--- src/main/java/com/designingmicroservices/dockerized/SwaggerConfig.java
+++ src/main/java/com/designingmicroservices/dockerized/SwaggerConfig.java
@@ -14,7 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.designingmicroservices.dockerized.controller"))
 				.paths(PathSelectors.any()).build();
 	}
 }
--- src/main/java/com/designingmicroservices/dockerized/controller/DockerizeController.java
+++ src/main/java/com/designingmicroservices/dockerized/controller/DockerizeController.java
@@ -5,13 +5,14 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class DockerizeController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	@RequestMapping("/greeting")
+	@RequestMapping(value="/greeting", method = RequestMethod.GET)
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
